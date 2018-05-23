01227a05c701b497da6befe85c1bfd78c252306d
thiru <e.thiru20@gmail.com>
2018-05-23 15:48:42 +0530

    cordova with socketio using browser
    cordova with socketio using browser

---
+++ chat/platforms/browser/www/js/nodeClient.js
@@ -0,0 +1,21 @@
+//var socket = io.connect('http://192.168.0.105:1034');
+  var socket = io.connect('http://localhost:1034');
+
+
+$( "#SendButton" ).click( function() {
+	var msg = $( "#messageText" ).val();
+	
+	socket.emit( 'message', { message: msg } );
+	return false;
+});
+
+socket.on( 'message', function( data ) {
+	var actualContent = $( "#incomingMessages" ).html();
+	var newMsgContent = '<li>' + data.message + '</li>';
+	var content = actualContent+newMsgContent;
+	$( "#incomingMessages" ).html( content );
+	$('#messageText').val('');
+	var objDiv = document.getElementById("incomingMessages");
+	objDiv.scrollTop = objDiv.scrollHeight;
+			
+});
